f7692dc7ec3497f40c7598f574a3926bc2231dc0
Tetsuya Mori <t2y3141592@gmail.com>
2016-02-02 21:27:43 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,251 @@
+# gulp-i18n-leverage
+
+Merge changes in default JSON into localized JSON for [i18n-behavior](https://github.com/t2ym/i18n-behavior)
+
+## Features
+
+- Merge changes in next default JSON from current default JSON into next localized JSON with the help of [rfc6902](https://github.com/chbrown/rfc6902)
+- Put meta information on merged changes in meta property in JSON for ease of translation
+- Output bundles object to merge all the UI texts into JSON per locale
+- Export the merged localized JSON as files
+
+## Install
+
+```
+    npm install --save-dev gulp-i18n-leverage
+```
+
+## Workflow
+
+Build tasks from source to dist:
+
+1. Scan task with [gulp-i18n-preprocess](https://github.com/t2ym/gulp-i18n-preprocess)
+1. Preprocess task with [gulp-i18n-preprocess](https://github.com/t2ym/gulp-i18n-preprocess)
+1. Leverage task 
+  - Update localized JSON files by merging differences in default JSON from the previous build
+  - Put them in dist
+  - Merge all the UI texts into bundles object
+1. Bundles task with `fs.writeFileSync()`
+  - Generate default bundled JSON file `bundle.json` from the bundles object
+  - Generate per-locale bundled JSON files `bundle.*.json` from the bundles object
+  - Put them in dist
+1. Feedback task with [gulp-i18n-preprocess](https://github.com/t2ym/gulp-i18n-preprocess)
+  - Update default and localized JSON files in source to commit them later by a developer
+
+## Usage
+
+### Default options
+
+Sample to show default options:
+
+```javascript
+    var gulp = require('gulp');
+    var i18nLeverage = require('gulp-i18n-leverage');
+
+    gulp.task('leverage', function () {
+      return gulp.src([ 'app/**/locales/*.json' ]) // input localized JSON files in source
+        .pipe(i18nLeverage({
+          jsonSpace: 2, // default JSON format with 2 spaces
+          srcPath: 'app', // default path to source root
+          distPath: 'dist', // default path to dist root to fetch next default JSON files
+          bundles: {} // default output bundles object is empty
+        }))
+        .pipe(gulp.dest('dist')); // path to output next localized JSON files
+    });
+```
+
+### Scan task
+
+Input: custom element HTMLs in source
+Output: attributesRepository object in gulpfile.js
+
+```javascript
+    var gulp = require('gulp');
+    var i18nPreprocess = require('gulp-i18n-preprocess');
+
+    // Global object to store localizable attributes repository
+    var attributesRepository = {};
+
+    // Scan HTMLs and construct localizable attributes repository
+    gulp.task('scan', function () {
+      return gulp.src([ 'app/elements/**/*.html' ]) // input custom element HTMLs
+        .pipe(i18nPreprocess({
+          constructAttributesRepository: true, // construct attributes repository
+          attributesRepository: attributesRepository, // output object
+          srcPath: 'app', // path to source root
+          attributesRepositoryPath: 
+            'bower_components/i18n-behavior/i18n-attr-repo.html', // path to i18n-attr-repo.html
+          dropHtml: true // drop HTMLs
+        })) 
+        .pipe(gulp.dest('dist/elements')); // no outputs; dummy output path
+    });
+```
+
+### Preprocess task
+
+Input: custom element HTMLs and non-custom-element HTMLs in source
+Output: preprocessed HTMLs and default JSON files in dist
+
+```javascript
+    var gulp = require('gulp');
+    var merge = require('merge-stream');
+    var i18nPreprocess = require('gulp-i18n-preprocess');
+
+    // Global object to store localizable attributes repository
+    var attributesRepository; // constructed attributes repository
+
+    // Other standard pipes such as crisper / minification / uglification are omitted for explanation
+    gulp.task('preprocess', function () {
+      var elements = gulp.src([ 'app/elements/**/*.html' ]) // input custom element HTMLs
+        .pipe(i18nPreprocess({
+          replacingText: true, // replace UI texts with {{annotations}}
+          jsonSpace: 2, // JSON format with 2 spaces
+          srcPath: 'app', // path to source root
+          attributesRepository: attributesRepository // input attributes repository
+        })))
+        .pipe(gulp.dest('dist/elements')); // output preprocessed HTMLs and default JSON files to dist
+
+      var html = gulp.src([ 'app/**/*.html', '!app/{elements,test}/**/*.html' ]) // non-custom-element HTMLs
+        .pipe(i18nPreprocess({
+          replacingText: true, // replace UI texts with {{annotations}}
+          jsonSpace: 2, // JSON format with 2 spaces
+          srcPath: 'app', // path to source root
+          force: true, // force processing even without direct i18n-behavior.html import
+          attributesRepository: attributesRepository // input attributes repository
+         }))
+        .pipe(gulp.dest('dist'));
+
+      return merge(elements, html)
+        .pipe($.size({title: 'copy'}));
+    });
+```
+
+### Leverage task
+
+Input:
+  - Current localized JSON files in source
+  - Current default JSON files in source
+  - Next default JSON files in dist
+
+Output:
+  - Next localized JSON files in dist
+  - Bundles object in gulpfile.js
+
+```javascript
+    var gulp = require('gulp');
+    var i18nLeverage = require('gulp-i18n-leverage');
+
+    var bundles = {};
+
+    gulp.task('leverage', function () {
+      return gulp.src([ 'app/**/locales/*.json' ]) // input localized JSON files in source
+        .pipe(i18nLeverage({
+          jsonSpace: 2, // JSON format with 2 spaces
+          srcPath: 'app', // path to source root
+          distPath: 'dist', // path to dist root to fetch next default JSON files
+          bundles: bundles // output bundles object
+        }))
+        .pipe(gulp.dest('dist')); // path to output next localized JSON files
+    });
+```
+
+### Bundles task
+
+Input: Bundles object in gulpfile.js
+Output: Bundles JSON files in dist
+
+```javascript
+    var gulp = require('gulp');
+    var fs = require('fs');
+    var JSONstringify = require('json-stringify-safe');
+
+    var bundles; // constructed bundles
+
+    gulp.task('bundles', function (callback) {
+      var DEST_DIR = 'dist';
+      var localesPath = DEST_DIR + '/locales';
+      var stats = fs.statSync(localesPath);
+
+      if (!(stats && stats.isDirectory())) {
+        fs.mkdirSync(localesPath);
+      }
+      for (var lang in bundles) {
+        bundles[lang].bundle = true;
+        if (lang) {
+          fs.writeFileSync(localesPath + '/bundle.' + lang + '.json', 
+                            JSONstringify(bundles[lang], null, 2));
+        }
+        else {
+          fs.writeFileSync(DEST_DIR + '/bundle.json', 
+                            JSONstringify(bundles[lang], null, 2));
+        }
+      }
+      callback();
+    });
+```
+
+### Feedback task
+
+Input:
+  - Next localized JSON files in dist
+  - Custom element HTMLs
+  - Non-custom-element HTMLs
+Output:
+  - Overwritten localized JSON files in source
+  - Overwritten default JSON files in source
+
+Outputs are ready to commit in the repository
+
+```
+    var gulp = require('gulp');
+    var merge = require('merge-stream');
+    var i18nPreprocess = require('gulp-i18n-preprocess');
+
+    // Only applicable to development builds; Skip it in production builds
+    gulp.task('feedback', function () {
+      // Copy from dist
+      var locales = gulp.src([ 'dist/**/locales/*.json', '!dist/locales/bundle.*.json'])
+        .pipe(gulp.dest('app'));
+
+      // Regenerate default JSON files
+      var elementDefault = gulp.src([ 'app/elements/**/*.html' ])
+        .pipe(i18nPreprocess({
+          replacingText: false,
+          jsonSpace: 2,
+          srcPath: 'app',
+          dropHtml: true,
+          attributesRepository: attributesRepository
+        }))
+        .pipe(gulp.dest('app/elements'));
+
+      // Regenerate default JSON files for non-custom-element HTMLs, i.e., i18n-dom-bind
+      var appDefault = gulp.src([ 'app/**/*.html', '!app/{elements,test}/**/*.html' ])
+        .pipe(i18nPreprocess({
+          replacingText: false,
+          jsonSpace: 2,
+          srcPath: 'app',
+          force: true,
+          dropHtml: true,
+          attributesRepository: attributesRepository
+        }))
+        .pipe(gulp.dest('app'));
+
+      return merge(locales, elementDefault, appDefault)
+        .pipe($.size({title: 'feedback'}));
+    });
+```
+
+## API
+
+`i18nLeverage(options)`
+
+### `options` object
+
+- jsonSpace: Number, default: 2 - JSON stringification parameter for formatting
+- srcPath: String, default: 'app' - Path to source root
+- distPath: String, default: 'dist' - Path to dist root to fetch next default JSON files
+- bundles: Object, default {} - Output bundles object
+
+## License
+
+[BSD-2-Clause](https://github.com/t2ym/gulp-i18n-leverage/blob/master/LICENSE.md)
---
+++ index.js
@@ -0,0 +1,427 @@
+/*
+@license https://github.com/t2ym/gulp-i18n-leverage/blob/master/LICENSE.md
+Copyright (c) 2016, Tetsuya Mori <t2y3141592@gmail.com>. All rights reserved.
+*/
+'use strict';
+
+var path = require('path');
+var fs = require('fs');
+var JSONstringify = require('json-stringify-safe');
+var rfc6902 = require('rfc6902');
+var deepcopy = require('deepcopy');
+var gutil = require('gulp-util');
+var through = require('through2');
+
+/**
+ * Gulp plugin to merge text changes in the default locale into a localized bundle.
+ *
+ * @namespace gulp-i18n-leverage
+ */
+module.exports = function(options) {
+  return through.obj(function (file, enc, callback) {
+    var jsonSpace = options && options.jsonSpace ? options.jsonSpace : 2;
+    var srcPath = options && options.srcPath ? options.srcPath : 'app';
+    var distPath = options && options.distPath ? options.distPath : 'dist';
+    var bundles = options && options.bundles ? options.bundles : {};
+
+    if (file.isNull()) {
+      return callback(null, file);
+    }
+
+    function deepMap(target, source, map) {
+      var value;
+      for (var prop in source) {
+        value = source[prop];
+        switch (typeof value) {
+        case 'string':
+        case 'number':
+        case 'boolean':        
+          target[prop] = map(value, prop);
+          break;
+        case 'object':
+          if (Array.isArray(value)) {
+            // TODO: cannot handle deep objects properly
+            target[prop] = target[prop] || [];
+            deepMap(target[prop], value, map);
+          }
+          else {
+            target[prop] = target[prop] || {};
+            deepMap(target[prop], value, map);
+          }
+          break;
+        case 'function':
+        case 'symbol':
+        case 'undefined':
+          target[prop] = value;
+          break;
+        default:
+          target[prop] = value;
+          break;
+        }
+      }
+    }
+
+    function getNextFallbackLanguage(lang) {
+      var nextFallbackLanguage = null;
+      if (lang && lang.length > 0) {
+        var parts = lang.split(/[-_]/);
+        if (parts.length >= 2) {
+          parts.pop();
+          nextFallbackLanguage = parts.join('-');
+        }
+      }
+      return nextFallbackLanguage;
+    }
+
+    function parsePath() {
+      var r = {};
+      r.path = file.path;
+      r.base = file.base;
+      r.srcBase = file.base.substr(0, file.base.length - 1);
+      r.distBase = r.srcBase.substr(0, r.srcBase.length - srcPath.length) + distPath;
+      r.urlPath = file.path.substr(r.srcBase.length);
+      var splitPath = r.urlPath.split('/locales/');
+      r.componentBase = splitPath[0];
+      r.resourcePath = r.componentBase + '/locales';
+      r.fileName = splitPath[1];
+      r.extName = path.extname(r.fileName);
+      var baseNameWithLang = r.fileName.substr(0, r.fileName.length - r.extName.length);
+      var splitBaseName = baseNameWithLang.split('.');
+      r.lang = splitBaseName.pop();
+      r.baseName = r.fileName.substr(0, r.fileName.length - r.extName.length - r.lang.length - 1);
+      r.defaultPath = r.componentBase + '/' + r.baseName + r.extName;
+      r.ancestorSrcPaths = [];
+      var lang = getNextFallbackLanguage(r.lang);
+      while (lang) {
+        r.ancestorSrcPaths.push(r.srcBase + r.resourcePath + '/' + r.baseName + '.' + lang + r.extName);
+        lang = getNextFallbackLanguage(lang);
+      }
+      r.srcDefaultPath = r.srcBase + r.defaultPath;
+      r.distDefaultPath = r.distBase + r.defaultPath;
+      return r;
+    }
+
+    function findPatch(patch, op, path) {
+      for (var i in patch) {
+        var item = patch[i];
+        if (item.op === op && item.path === path) {
+          return item;
+        }
+      }
+      return null;
+    }
+
+    function findValue(obj, path) {
+      var splitPath = path.split('/');
+      var value = obj;
+      for (var i = 1; i < splitPath.length; i++) {
+        value = value[splitPath[i]];
+      }
+      return value;
+    }
+
+    function fragmentObject(obj, path) {
+      var fragments = [];
+      var op;
+      var i;
+      switch (typeof obj) {
+      case 'object':
+        if (Array.isArray(obj)) {
+          for (i = 0; i < obj.length; i++) {
+            fragments = fragments.concat(fragmentObject(obj[i], path + '/' + i));
+          }
+        }
+        else {
+          for (i in obj) {
+            fragments = fragments.concat(fragmentObject(obj[i], path + '/' + i));
+          }
+        }
+        break;
+      case 'string':
+      case 'number':
+      case 'boolean':
+      case 'function':
+      case 'symbol':
+      case 'undefined':
+      default:
+        op = {};
+        op[path] = obj;
+        fragments = [ op ];
+        break;
+      }
+      return fragments;
+    }
+
+    function fragmentOperation(rawOp) {
+      var operations = [];
+      switch (rawOp.op) {
+      case 'add':
+      case 'remove':
+      case 'replace':
+        if (typeof rawOp.value === 'object') {
+          fragmentObject(rawOp.value, rawOp.path).forEach(function (op) {
+            for (var p in op) {
+              operations.push({ op: rawOp.op, path: p, value: op[p] });
+            }
+          });
+        }
+        else {
+          operations.push(rawOp);
+        }
+        break;
+      case 'move':
+      case 'copy':
+        if (typeof rawOp.value === 'object') {
+          fragmentObject(rawOp.value, '').forEach(function (op) {
+            for (var p in op) {
+              operations.push({ op: rawOp.op, from: rawOp.from + p, path: rawOp.path + p });
+            }
+          });
+        }
+        else {
+          operations.push(rawOp);
+        }
+        break;
+      case 'test':
+      default:
+        operations.push(rawOp);
+        break;
+      }
+      return operations;
+    }
+
+    function fragmentPatch(patch) {
+      var fragmented = [];
+      for (var i = 0; i < patch.length; i++) {
+        fragmented = fragmented.concat(fragmentOperation(patch[i]));
+      }
+      return fragmented;
+    }
+
+    function minimizePatch(patch, prev, current) {
+      var minimized = [];
+      var valueOpMap = {};
+      var serialized;
+      var i;
+      var item, item2;
+      var removed;
+
+      // construct valueOpMap
+      for (i in patch) {
+        item = patch[i];
+        if (item.op === 'remove') {
+          item.value = findValue(prev, item.path);
+        }
+        serialized = JSONstringify(item.value);
+        valueOpMap[serialized] = valueOpMap[serialized] || [];
+        valueOpMap[serialized].push(i);
+      }
+
+      // convert remove/add pairs to move operations
+      for (i in valueOpMap) {
+        if (valueOpMap[i].length === 2) {
+          item = patch[valueOpMap[i][0]];
+          if (item.op === 'add') {
+            item2 = item;
+            item = patch[valueOpMap[i][1]];
+          }
+          else {
+            item2 = patch[valueOpMap[i][1]];
+          }
+          if (item.op === 'remove' && item2.op === 'add') {
+            // convert them to move operation
+            item.op = 'move';
+            item.from = item.path;
+            item.path = item2.path;
+            delete item.value;
+            item2.op = 'noop';
+          }
+        }
+      }
+
+      // construct minimized patch
+      for (i in patch) {
+        item = patch[i];
+        switch (item.op) {
+        case 'add':
+        case 'remove':
+        case 'replace':
+        case 'move':
+        case 'copy':
+          minimized.push(item);
+          break;
+        case 'test':
+        default:
+          break;
+        }
+      }
+      return minimized;
+    }
+
+    function updateMetaTodo(current, prev)
+    {
+      var meta = prev.meta || {};
+      var prevClone = deepcopy(prev);
+      var currentClone = deepcopy(current);
+      var patch;
+      var i, j;
+      delete prevClone.meta;
+      delete currentClone.meta;
+      current.meta.todo = [];
+      patch = rfc6902.createPatch(prevClone, currentClone);
+      patch = minimizePatch(patch, prevClone, currentClone);
+      //console.log('fragmenting ======================================================================');
+      //console.log(JSONstringify(patch, null, 2));
+      patch = fragmentPatch(patch);
+      //console.log(JSONstringify(patch, null, 2));
+      meta.todo = meta.todo || [];
+      for (i = 0; i < patch.length; i++) {
+        switch (patch[i].op) {
+        case 'add':
+        case 'replace':
+          for (j = 0; j < meta.todo.length; j++) {
+            if (meta.todo[j].path === patch[i].path) {
+              meta.todo[j].op = 'noop';
+              break;
+            }
+          }
+          meta.todo.push(patch[i]);
+          break;
+        case 'remove':
+          for (j = 0; j < meta.todo.length; j++) {
+            if (meta.todo[j].path === patch[i].path) {
+              meta.todo[j].op = 'noop';
+            }
+            else if (meta.todo[j].path.indexOf(patch[i].path + '/') === 0) {
+              meta.todo[j].op = 'noop';
+            }
+          }
+          break;
+        case 'move':
+          for (j = 0; j < meta.todo.length; j++) {
+            if (meta.todo[j].path === patch[i].from) {
+              meta.todo[j].path = patch[i].path;
+            }
+            else if (meta.todo[j].path.indexOf(patch[i].from + '/') === 0) {
+              meta.todo[j].path = patch[i].path + meta.todo[j].path.substr(patch[i].from.length);
+            }
+          }
+          break;
+        case 'copy': // TODO: should not happen for now
+        case 'test':
+        default:
+          break;
+        }
+      }
+      for (i = meta.todo.length - 1; i >= 0; i--) {
+        if (meta.todo[i].op === 'noop') {
+          meta.todo.splice(i, 1);
+        }
+      }
+      current.meta = meta;
+      return current;
+    }
+
+    function update(paths, contents) {
+      var prevDefault;
+      var currentDefault;
+      var prevLocalized;
+      var prevLocalizedOriginal;
+      var currentLocalized;
+      var prevAncestors = [];
+      var patchStatus;
+      var i;
+      try {
+        prevDefault = JSON.parse(fs.readFileSync(paths.srcDefaultPath, 'utf8'));
+      }
+      catch (e) {
+        prevDefault = {}; // presumably file not found
+      }
+      try {
+        currentDefault = JSON.parse(fs.readFileSync(paths.distDefaultPath, 'utf8'));
+      }
+      catch (e) {
+        currentDefault = {}; // presumably file not found
+      }
+      for (i = 0; i < paths.ancestorSrcPaths.length; i++) {
+        try {
+          prevAncestors[i] = JSON.parse(fs.readFileSync(paths.ancestorSrcPaths[i], 'utf8'));
+        }
+        catch (e) {
+          prevAncestors[i] = null; // presumably file not found
+        }
+      }
+
+      try {
+        prevLocalized = JSON.parse(contents);
+      }
+      catch (e) {
+        prevLocalized = {};
+      }
+      prevLocalizedOriginal = deepcopy(prevLocalized);
+
+      var patch = rfc6902.createPatch(prevDefault, currentDefault);
+      patch = minimizePatch(patch, prevDefault, currentDefault);
+
+      for (i = 0; i < prevAncestors.length; i++) {
+        if (prevAncestors[i]) {
+          rfc6902.applyPatch(prevAncestors[i], patch);
+        }
+      }
+
+      patchStatus = rfc6902.applyPatch(prevLocalized, patch);
+      for (i = 0; i < patchStatus.length; i++) {
+        if (patchStatus[i]) {
+          console.log('warning: ' + patchStatus[i].name +
+                                  ' for ' + JSONstringify(patch[i]));
+        }
+      }
+
+      currentLocalized = deepcopy(currentDefault);
+
+      for (i = 0; i < prevAncestors.length; i++) {
+        if (prevAncestors[i]) {
+          deepMap(currentLocalized, prevAncestors[i], function (value) { return value; });
+        }
+      }
+
+      deepMap(currentLocalized, prevLocalized, function (value) { return value; });
+
+      updateMetaTodo(currentLocalized, prevLocalizedOriginal);
+
+      //console.log(JSONstringify(patch, null, 2));
+      bundles[paths.lang] = bundles[paths.lang] || {};
+      bundles[paths.lang][paths.baseName] = deepcopy(currentLocalized);
+      bundles[''] = bundles[''] || {};
+      bundles[''][paths.baseName] = deepcopy(currentDefault);
+      return JSONstringify(currentLocalized, null, jsonSpace);
+    }
+
+    function doLeverage() {
+      if (file.isNull()) {
+        callback(null, file);
+        return;
+      }
+
+      if (file.isStream()) {
+        callback(new gutil.PluginError('leverage', 'Streaming not supported'));
+        return;
+      }
+
+      if (file.isBuffer()) {
+        var contents = String(file.contents);
+        var paths = parsePath();
+        //console.log(paths);
+        var result = update(paths, contents);
+        if (result) {
+          file.contents = new Buffer(result);
+          return callback(null, file);
+        }
+      }
+
+      callback(null, file);
+    }
+
+    doLeverage();
+  });
+};
